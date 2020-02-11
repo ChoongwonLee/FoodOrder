27600b6a85f1d3b7960a1bdba185c2e7a92b86ef
ChoongwonLee <bigstep810@gmail.com>
2020-02-11 16:55:27 -0500

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./db/mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+// Connect MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to food order API...' });
