cf01965db4fe80163013aa0336fc508c9318fd24
ChoongwonLee <bigstep810@gmail.com>
2020-03-12 19:20:37 -0400

    Setup for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./db/mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -21,10 +22,18 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/users'));
-// app.use('/api/admin', require('./routes/admin'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/menus', require('./routes/menus'));
-// app.use('/api/customers', require('./routes/customers'));
 app.use('/api/orders', require('./routes/order'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
